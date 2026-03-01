c69f4c87d1f2a0c1e1a7fcd47c07460f7a53e425
Adisdzaky <adisdzakyrivai@gmail.com>
2026-03-01 10:28:58 +0700

    Update index.php

--- api/index.php
+++ api/index.php
@@ -1,9 +1,5 @@
         
 <?php
-// CORS headers (opsional, jika ingin diakses dari domain lain)
-header("Access-Control-Allow-Origin: *");
-header("Access-Control-Allow-Methods: POST, GET, OPTIONS");
-header("Access-Control-Allow-Headers: Content-Type");
 
 // Tangani preflight OPTIONS
 if ($_SERVER['REQUEST_METHOD'] === 'OPTIONS') {
